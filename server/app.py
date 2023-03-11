7534ffccad65992752e32614d8e4f2424a3916e9
Prannaya <prannayagupta@gmail.com>
2023-03-11 20:19:19 +0800

    servering off

--- server/app.py
+++ server/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 import pandas as pd
 from pandasql import sqldf
+from flask_cors import CORS
 
 nush_students = pd.read_csv("data/nush_students.csv")
 nush_teachers = pd.read_csv("data/nush_teachers.csv")
@@ -8,6 +9,8 @@ projects = pd.read_csv("data/projects.csv")
 works_on = pd.read_csv("data/works_on.csv")
 
 app = Flask(__name__)
+app.secret_key = "super_secret_key"
+CORS(app)
 
 
 def getStudentDetails(email):
@@ -48,23 +51,16 @@ def projectsByStudent():
         )
         """
     ).groupby(["pcode", "title", "teacherEmail", "year"]).studentEmail.agg(list).reset_index().T.to_dict().values()))
-    res.headers.add('Access-Control-Allow-Origin', '*')
+    #res.headers.add('Access-Control-Allow-Origin', '*')
     return res
 
 
 @app.route("/student", methods=["GET"])
 def student():
     email = request.args.get("email", "h1810124@nushigh.edu.sg", str)
-    res = sqldf(
-        f"""
-        SELECT *
-        FROM nush_students
-        WHERE email = '{email}'
-        """
-    ).iloc[0].to_dict()
-    res["graduationYear"] = int(res["graduationYear"])
-    res = jsonify(res)
-    res.headers.add('Access-Control-Allow-Origin', '*')
+    
+    res = jsonify(getStudentDetails(email))
+    #res.headers.add('Access-Control-Allow-Origin', '*')
     return res
 
 
@@ -79,7 +75,7 @@ def teacher():
         """
     ).iloc[0].to_dict()
     res = jsonify(res)
-    res.headers.add('Access-Control-Allow-Origin', '*')
+    #res.headers.add('Access-Control-Allow-Origin', '*')
     return res
 
 
