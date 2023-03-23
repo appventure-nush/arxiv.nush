9d1d43c6ee9e0e7f27eb1618884a81cd75aed9a5
Prannaya <prannayagupta@gmail.com>
2023-03-23 23:50:53 +0800

    painful database errors

--- server/app.py
+++ server/app.py
@@ -1,83 +1,80 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, session
 import pandas as pd
-from pandasql import sqldf
 from flask_cors import CORS
-
-nush_students = pd.read_csv("data/nush_students.csv")
-nush_teachers = pd.read_csv("data/nush_teachers.csv")
-projects = pd.read_csv("data/projects.csv")
-works_on = pd.read_csv("data/works_on.csv")
+from flask_mysqldb import MySQL
+import MySQLdb.cursors
+import re
+from database import Database
 
 app = Flask(__name__)
 app.secret_key = "super_secret_key"
 CORS(app)
+database = Database(app)
 
 
-def getStudentDetails(email):
-    res = sqldf(
-        f"""
-        SELECT *
-        FROM nush_students
-        WHERE email = '{email}'
-        """
-    ).iloc[0].to_dict()
-    res["graduationYear"] = int(res["graduationYear"])
-    return res
+# def getProjects(email):
+#     projects = sqldf(
+#         f"""
+#         SELECT *
+#         FROM projects
+#         WHERE "{email}" in (
+#             SELECT studentEmail
+#             FROM works_on
+#             WHERE works_on.pcode = projects.pcode
+#         )
+#         """
+#     ).to_dict('records')
 
-def getProjects(email):
-    projects = sqldf(
-        f"""
-        SELECT *
-        FROM projects
-        WHERE "{email}" in (
-            SELECT studentEmail
-            FROM works_on
-            WHERE works_on.pcode = projects.pcode
-        )
-        """
-    ).to_dict('records')
-
-@app.route("/projectsByStudent", methods=["GET"])
-def projectsByStudent():
-    email = request.args.get("email@nushigh.edu.sg", "h1810124", str)
-    res = jsonify(list(sqldf(
-        f"""
-        SELECT *
-        FROM projects NATURAL JOIN works_on
-        WHERE "{email}" in (
-            SELECT studentEmail
-            FROM works_on
-            WHERE works_on.pcode = projects.pcode
-        )
-        """
-    ).groupby(["pcode", "title", "teacherEmail", "year"]).studentEmail.agg(list).reset_index().T.to_dict().values()))
-    #res.headers.add('Access-Control-Allow-Origin', '*')
-    return res
+# @app.route("/projectsByStudent", methods=["GET"])
+# def projectsByStudent():
+#     email = request.args.get("email@nushigh.edu.sg", "h1810124", str)
+#     res = jsonify(list(sqldf(
+#         f"""
+#         SELECT *
+#         FROM projects NATURAL JOIN works_on
+#         WHERE "{email}" in (
+#             SELECT studentEmail
+#             FROM works_on
+#             WHERE works_on.pcode = projects.pcode
+#         )
+#         """
+#     ).groupby(["pcode", "title", "teacherEmail", "year"]).studentEmail.agg(list).reset_index().T.to_dict().values()))
+#     #res.headers.add('Access-Control-Allow-Origin', '*')
+#     return res
 
 
 @app.route("/student", methods=["GET"])
 def student():
-    email = request.args.get("email", "h1810124@nushigh.edu.sg", str)
-    
-    res = jsonify(getStudentDetails(email))
-    #res.headers.add('Access-Control-Allow-Origin', '*')
+    id = request.args.get("id", "h1810124", str)
+    res = jsonify(database.student(id))
     return res
-
 
 @app.route("/teacher", methods=["GET"])
 def teacher():
     email = request.args.get("email", "nhscsp@nushigh.edu.sg", str)
-    res = sqldf(
-        f"""
-        SELECT *
-        FROM nush_teachers
-        WHERE email = '{email}'
-        """
-    ).iloc[0].to_dict()
-    res = jsonify(res)
-    #res.headers.add('Access-Control-Allow-Origin', '*')
+    res = jsonify(database.teacher(email))
     return res
 
+@app.route("/institute", methods=["GET"])
+def institute():
+    id = request.args.get("id", "NUS", str)
+    res = jsonify(database.institute(id))
+    return res
+
+@app.route("/authors", methods=["GET"])
+def authors():
+    pcode = request.args.get("pcode", "23.018.NUSH.CS", str)
+    res = jsonify(database.projectMembers(pcode))
+    return res
+
+@app.route("/projects", methods=["GET"])
+def 
+
+@app.route("/coauthors", methods=["GET"])
+def coauthors():
+    email = request.args.get("email", "h1810124@nushigh.edu.sg", str)
+    res = jsonify(database.coauthors(email))
+    return res
 
 @app.route('/')
 def hello_world():
---
+++ server/database.py
@@ -0,0 +1,66 @@
+from flask_mysqldb import MySQL
+import MySQLdb.cursors
+
+
+class Database:
+    def __init__(self, app):
+        app.config['MYSQL_HOST'] = 'localhost'
+        app.config['MYSQL_USER'] = 'root'
+        app.config['MYSQL_PASSWORD'] = 'admin'
+        app.config['MYSQL_DB'] = 'arxiv'
+        self.app = app
+        self.mysql = MySQL(app)
+
+    @property
+    def connection(self):
+        return self.mysql.connection
+
+    @property
+    def cursor(self):
+        return self.mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+    
+    def execute(self, query, args=None):
+        cur = self.cursor
+        cur.execute(query, args)
+    
+    def queryAll(self, query, args=None):
+        cur = self.cursor
+        cur.execute(query, args)
+        return cur.fetchall()
+    
+    def queryOne(self, query, args=None):
+        cur = self.cursor
+        cur.execute(query, args)
+        return cur.fetchone()
+
+    def close(self):
+        self.connection.close()
+        
+    def isStudent(self, sid):
+        return len(self.queryAll("SELECT email FROM NUSHStudent WHERE nush_sid = %s", (sid, ))) > 0
+        
+    def student(self, sid):
+        query = "SELECT Student.email, name, gradYear, nush_sid, pfp, pwd FROM NUSHStudent INNER JOIN Student WHERE NUSHStudent.nush_sid = %s AND NUSHStudent.email = Student.email"
+        return self.queryOne(query, (sid, ))
+    
+    def teacher(self, email):
+        return self.queryOne("SELECT * FROM Institution WHERE email = %s", (email, ))
+    
+    def institute(self, instId):
+        return self.queryOne("SELECT * FROM Institution WHERE instId = %s", (instId, ))
+    
+    def projectMembers(self, pcode):
+        query = "SELECT studentEmail email FROM Works_On WHERE pcode = %s"
+        return [self.student(i["email"]) for i in self.queryAll(query, (pcode,))]
+    
+    def project(self, pcode):
+        return self.queryOne("SELECT * FROM Project WHERE pcode = %s", (pcode, ))
+    
+    def studentProjects(self, email):
+        return self.queryAll("SELECT pcode FROM Works_On WHERE email = %s", (email, ))
+    
+    def coauthors(self, email):
+        query = "SELECT distinct other.studentEmail email FROM Works_On self, Works_On other WHERE self.pcode = other.pcode AND self.studentEmail = %s AND other.studentEmail <> self.studentEmail"
+        
+        return [self.student(i["email"]) for i in self.queryAll(query, (email,)) if self.isStudent(i["email"])]
+        
