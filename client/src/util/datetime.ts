70a591f575c3a2ba0d7c93b0db2e836b254ab176
Prannaya <prannayagupta@gmail.com>
2023-04-04 20:19:19 +0800

    remove vue 2 cuz screw vue 2

--- client/src/util/datetime.ts
+++
@@ -1,8 +0,0 @@
-export function convertTimestampToDate(timestamp: number): Date {
-  return new Date(timestamp * 1000);
-}
-
-export function formatDate(date: Date): string {
-  return date.getDate()  + "-" + (date.getMonth()+1) + "-" + date.getFullYear() + " " +
-    date.getHours() + ":" + date.getMinutes();
-}
