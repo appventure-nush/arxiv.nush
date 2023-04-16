c38c4935ba15085a5e3c85302e1030656f0d416b
Prannaya <prannayagupta@gmail.com>
2023-04-16 18:29:19 +0800

    MASS COMMIT: I finished frontend cuz ofc I did

---
+++ frontend/src/views/events/EventHome.vue
@@ -0,0 +1,202 @@
+<template>
+  <v-container fluid>
+  <v-row>
+  <!-- <div class="h-screen w-screen flex items-center justify-center">
+    <div class="w-10/12 h-full">
+      <div class="w-full mt-5 px-2"> -->
+        <v-col cols="12" md="2" class="ma-4">
+        <v-list class="text-overline" v-model:opened="open">
+          <v-list-subheader><b>Events</b></v-list-subheader>
+          <v-list-item class="text-overline text-left" href="/events">
+            <v-list-item-title class="text-overline">Home</v-list-item-title>
+          </v-list-item>
+
+          <v-list-group v-for="eventId in getEventIds(events)" :key="eventId" :value="eventId">
+          <template v-slot:activator="{ props }">
+            <v-list-item class="text-overline text-left"
+              v-bind="props"
+            >
+            <v-list-item-title class="text-overline text-center">{{eventId}}</v-list-item-title>
+          </v-list-item>
+          </template>
+            <v-list-item v-for="year in getYears(events, eventId)"
+              :key="year" :value="eventId"
+              :href="`/events/${eventId}/${year}`">
+              <v-breadcrumbs :items="['', `${year}`]"></v-breadcrumbs>
+            </v-list-item>
+          </v-list-group>
+          <v-list-item v-if="!appStore.isStudent && (appStore.user as Teacher).isAdmin">
+            <v-btn color="primary" @click="newEventVisible = true" block>Create</v-btn>
+          </v-list-item>
+        </v-list>
+
+
+        </v-col>
+        <v-col cols="12" md="9" class="ma-4">
+          <div class="ma-4">
+          <h1>Event Calendar</h1>
+
+          <p class="my-4">
+            Here is a summary of the various events held over the course of the year. The side bar should also give you a good understanding of the different events. For teacher administrators, you can create events here too.
+
+          </p>
+        </div>
+        <Calendar :events="calendarEvents">
+          <template #eventDialog="props">
+            <v-card v-if="props.eventDialogData && props.eventDialogData.title"
+              class="p-4 justify-center bg-gray-100">
+              <div class="text-sm md:text-base font-bold text-gray-700 text-center">{{ props.eventDialogData.title }}</div>
+              <div class="mt-5">
+                <div class="text-xs text-gray-700 font-medium space-y-2">
+                  <h6 class="items-center space-x-2"><v-icon>mdi-clock-outline</v-icon>&nbsp;&nbsp;{{ props.eventDialogData.time.start }}</h6>
+                  <h6 class="items-center space-x-2 font-medium italic"><v-icon>mdi-tag-outline</v-icon>&nbsp;&nbsp;{{ props.eventDialogData.tags }}</h6>
+                </div>
+
+
+                <vue-markdown class="w-full text-xs text-gray-700 mt-5" :source="props.eventDialogData.description" :options="viewOptions"></vue-markdown>
+
+                <v-card-actions>
+                  <v-btn class="bg-gray-300 hover:bg-gray-400 text-gray-700" size="small" @click="props.closeEventDialog">
+                      <v-icon>mdi-close</v-icon>&nbsp;Close
+                    </v-btn>
+
+                    <v-btn class="bg-purple-600 hover:bg-purple-700 text-white" size="small" :href="props.eventDialogData.url">
+                      See More
+                    </v-btn>
+                </v-card-actions>
+              </div>
+            </v-card>
+          </template>
+        </Calendar>
+      </v-col>
+      <!-- </div>
+
+    </div>
+
+  </div> -->
+
+  </v-row>
+  <NewEventDialog v-model:visible="newEventVisible"></NewEventDialog>
+</v-container>
+
+</template>
+
+<script lang="ts" setup>
+import { ref, onMounted, type Ref } from "vue";
+import Calendar from "@/components/calendar/Calendar.vue";
+import "@/assets/main.css";
+import VueMarkdown from 'vue-markdown-render'
+import { ResearchEvent, Event, dummyEvent } from '@/types/project';
+import { loadEvent, loadEvents } from '@/api/events';
+import NewEventDialog from "@/components/event/NewEventDialog.vue";
+import { useAppStore } from "@/store/app";
+import { Teacher } from "@/types/user";
+
+const appStore = useAppStore()
+
+const events: Ref<Event[]> = ref([])
+
+const open: Ref<string[]> = ref([])
+
+const newEventVisible = ref(false)
+
+function onlyUnique(value: any, index: number, array: any[]) {
+  return array.indexOf(value) === index;
+}
+
+function getEventIds(events: Event[]): string[] {
+  return events.map(event => event.eventId).filter(onlyUnique)
+}
+
+function getYears(events: Event[], eventId: string): number[] {
+  return events.filter(it => it.eventId == eventId).map(it => it.year)
+}
+
+const viewOptions = {
+  html:         true,        // Enable HTML tags in source
+  breaks:       true,        // Convert '\n' in paragraphs into <br>
+  linkify:      true,        // Autoconvert URL-like text to links
+  typographer:  true,
+  quotes: '“”‘’'
+}
+
+// all events data
+const calendarEvents = ref([
+  {
+    id: 5,
+    url: '/events/SSEF/2023',
+    title: "SSEF 2023",
+    time: {
+      start: '2023-03-07 00:00',
+      end: '2023-03-08 23:59'
+    },
+    description: "SSEF 2023 yay",
+    tags: '#competition',
+    competition: true,
+    conference: false
+  },
+  {
+    id: 6,
+    url: '/events/SSEF/2023',
+    title: "SSEF 2023",
+    time: {
+      start: '2023-03-07 00:00',
+      end: '2023-03-08 23:59'
+    },
+    description: "SSEF 2023 yay",
+    tags: '#competition',
+    competition: true,
+    conference: false
+  },
+  {
+    id: 8,
+    url: '/events/IRCSET/2023',
+    title: "IRCSET 2023",
+    time: {
+      start: '2023-03-07 00:00',
+      end: '2023-03-08 23:59'
+    },
+    description: "IRCSET 2023 yay",
+    tags: '#conference',
+    competition: false,
+    conference: true
+  },
+  {
+    id: 7,
+    url: '/events/SSEF/2023',
+    title: "SSEF 2023",
+    time: {
+      start: '2023-03-07 00:00',
+      end: '2023-03-08 23:59'
+    },
+    description: "SSEF 2023 yay",
+    tags: '#competition',
+    competition: true,
+    conference: false
+  },
+]);
+
+onMounted(() => {
+  loadEvents().then(res => {
+    events.value = res ?? events.value;
+
+    calendarEvents.value = events.value.map((it: Event, id: number) => {
+      return {
+        id,
+        url: `/events/${it.eventId}/${it.year}`,
+        title: `${it.name} (${it.year})`,
+        time: {
+          start: `${it.start_date.getFullYear()}-${it.start_date.getMonth() < 9 ? '0' : ''}${it.start_date.getMonth()+1}-${it.start_date.getDate() < 10 ? '0' : ''}${it.start_date.getDate()} 00:00`,
+          end: '2023-03-08 23:59'
+        },
+        description: (it.about.match(RegExp(/(^.*?[a-z]{2,}[.!?])\s+\W*[A-Z]/)) ?? ['', it.about])[1],
+        tags: it.isCompetition && it.isConference ? "#competition #conference" : it.isCompetition ? '#competition' : it.isConference ? '#conference' : '',
+        competition: it.isCompetition,
+        conference: it.isConference
+      }
+    })
+  })
+
+})
+
+</script>
---
+++ frontend/src/views/profile/StudentView.vue
@@ -0,0 +1,259 @@
+<template>
+  <v-container fluid>
+    <v-row>
+      <v-col cols="3">
+        <v-card class="mx-auto pa-4 text-center">
+            <!-- <v-flex class="text-overline"> -->
+                <div class="mx-4 d-flex text-overline">
+                    {{ isNush ? (student as Student).nush_sid : 'External' }} <v-spacer /> {{ isNush ? `Y${6 + (new Date()).getFullYear() - (student as Student).gradYear ?? 0}` : (student as ExternalStudent).schId }}
+                </div>
+            <!-- </v-flex> -->
+            <FileSelector
+                v-if="isOwn && isNush"
+                style="margin-top:20px"
+                accept-extensions=".png,.jpg,.gif"
+                :multiple="false"
+                :max-file-size="15 * 1024 * 1024"
+                @validated="handleFilesValidated"
+                @changed="handleFilesChanged">
+            <v-avatar v-ripple size="200px" class="justify-center">
+              <v-img
+                :aspect-ratio="1"
+                :width="250"
+                :src="`data:image/png;base64,${student.pfp}`"
+                class="justify-center"
+                cover
+                alt="Click to select avatar"
+              ></v-img>
+            </v-avatar>
+          </FileSelector>
+          <v-avatar v-else size="200px" class="justify-center">
+              <v-img
+                :aspect-ratio="1"
+                :width="250"
+                :src="isNush ? `data:image/png;base64,${(student as Student).pfp}` : default_image"
+                class="justify-center"
+                cover
+                alt="Click to select avatar"
+              ></v-img>
+            </v-avatar>
+
+            <v-card-title class="justify-center text-wrap">{{ student.name }}</v-card-title>
+
+            <v-card-text v-if="!editAbout && isNush" class="justify-center" @click="if(isOwn) {tempAbout = student.about; editAbout = true;}">
+              {{  student.about ? student.about : "No About Inserted" }}
+            </v-card-text>
+            <v-text-field v-if="editAbout && isNush" clearable v-model="tempAbout" v-on:keyup.enter="editAbout = false; updateAbout()"></v-text-field>
+
+            <v-list class="text-left">
+              <v-list-subheader>{{isNush ? 'Common Collaborators' : 'Teacher Contact'}}</v-list-subheader>
+              <v-list-item
+                v-for="member in coauthors" :key="member.email"
+                :value="member.name"
+                :prepend-avatar="'nush_sid' in member ? (`data:image/png;base64,${member.pfp}` ?? '') : ''"
+                :href="`/students/${member.nush_sid}`"
+                active-color="primary"
+              >
+                <v-list-item-title>{{ member.name }}</v-list-item-title>
+                <v-list-item-subtitle>{{ member.nush_sid }}</v-list-item-subtitle>
+              </v-list-item>
+              <v-list-item v-if="!isNush"
+                :prepend-avatar="default_image"
+                :href="`/teachers/${(student as ExternalStudent).emergencyEmail}`"
+              >
+                <v-list-item-title>{{ (student as ExternalStudent).teacherName }}</v-list-item-title>
+                <v-list-item-subtitle class="text-wrap">{{ (student as ExternalStudent).emergencyEmail }}</v-list-item-subtitle>
+            </v-list-item>
+            </v-list>
+
+        </v-card>
+      </v-col>
+      <v-col>
+
+        <div class="mx-auto pa-4" height="100%">
+                <div class="mx-4 d-flex text-overline">
+                    Projects Done <v-spacer />
+                    <span :style="{'color': defaults.prefCardView ? 'black': 'primary !important'}">Table View</span><v-switch v-model="defaults.prefCardView" density='compact' label="" style="flex: none; margin-top:-5px; margin-right:5px;margin-left: 5px;" inset></v-switch><span :style="{'color': defaults.prefCardView ? 'primary !important': 'black'}">Card View</span>
+                </div>
+
+          <div class="pa-8">
+          <v-text-field
+            v-model="search"
+            append-icon="mdi-magnify"
+            label="Search"
+            single-line
+            hide-details
+          ></v-text-field>
+          </div>
+          <v-row dense v-if="defaults.prefCardView">
+            <v-col :cols="6"  v-for="project in searchProjects()" :key="project.pcode" style="padding-bottom: 20px;">
+              <v-card class="mx-auto pa-4" max-width="500px" height="100%">
+                <v-flex class="text-overline">
+                    <div class="mx-4 d-flex">
+                        {{ project.pcode }} <v-spacer /> {{ project.year }}
+                    </div>
+                </v-flex>
+                <v-card-title class="text-wrap" style="word-break: normal;">
+                    {{ project.title }}
+                </v-card-title>
+                <!-- <v-card-subtitle> -->
+                <p class="text-wrap mx-4" v-html="names(project)"></p>
+                <!-- </v-card-subtitle> -->
+                <v-card-actions>
+                  <v-btn
+                      color="primary" variant="elevated"
+                      :href="'/projects/'+project.pcode">
+                      Open
+                  </v-btn>
+                </v-card-actions>
+              </v-card>
+            </v-col>
+          </v-row>
+          <v-data-table
+            :headers="headers"
+            :items="projects"
+            v-if="!defaults.prefCardView"
+            item-value="name"
+            multi-sort
+            class="elevation-1"
+            loading-text="Loading... Please wait"
+            :footer-props="{showFirstLastPage: true, firstIcon: 'mdi-arrow-collapse-left', lastIcon: 'mdi-arrow-collapse-right'}"
+            :search="search"
+            v-model:sort-by="sortBy"
+          >
+            <template v-slot:item.title="{ item }">
+              <a :href="`/projects/${item.raw.pcode}`">{{ item.raw.title }}</a>
+            </template>
+            <template v-slot:item.members="{ item }">
+              <div v-html="names(item.raw)"></div>
+            </template>
+          </v-data-table>
+        </div>
+
+
+
+      </v-col>
+    </v-row>
+
+  </v-container>
+</template>
+<script lang="ts" setup>
+import { FsValidationResult } from 'vue-file-selector/dist';
+import fuzzysort from 'fuzzysort'
+import { ref, computed, type Ref, onMounted } from 'vue'
+import { Student, dummyStudent, ExternalStudent } from '@/types/user'
+import { Project } from "@/types/project"
+import { useRoute } from 'vue-router'
+import {loadStudent, loadCoauthors, updateStudent} from '@/api/user'
+import { loadProjects } from '@/api/projects'
+import { useDefaultsStore } from '@/store/defaults'
+import names from "@/util/names"
+import { emit } from 'process';
+import { useAppStore } from '@/store/app';
+import getBase64 from "@/util/getBase64";
+
+import default_image from "@/assets/default.png"
+
+const defaults = useDefaultsStore()
+const appStore = useAppStore()
+
+const route = useRoute()
+
+const sid = ref("")
+
+const student: Ref<Student | ExternalStudent> = ref(dummyStudent());
+
+const isNush = computed(() => ('nush_sid' in student.value))
+
+const projects: Ref<Project[]> = ref([])
+
+const coauthors: Ref<Student[]> = ref([])
+
+const isOwn = computed(() => appStore.user?.email == student.value.email)
+
+const editAbout = ref(false)
+const tempAbout = ref("")
+
+function updateGeneralStudent() {
+  updateStudent(student.value.nush_sid, student.value.about, student.value.pfp).then(
+    res => student.value = res ?? student.value
+  )
+}
+
+function updateAbout() {
+  student.value.about = tempAbout.value;
+  updateGeneralStudent()
+}
+
+const search = ref("")
+
+const sortBy = ref([{ key: 'pcode', order: 'asc' }])
+
+const headers = ref([
+  {title: "Project Code", value: "pcode", key: "pcode", sortable: true},
+  {title: "Title", value: "title", key: "title", sortable: true},
+  {title: "Members", value: "members", key: "members", sortable: false},
+  {title: "Year", value: "year", key: "year", sortable: true}
+])
+
+function searchProjects(): Project[] {
+  return search.value.length == 0 ? projects.value : fuzzysort.go(search.value, projects.value.map(
+  (it) => {
+    return {
+      ...it,
+      combinedMembers: names(it)
+    }
+  }
+  ), {
+    keys: ['title', 'pcode', "combinedMembers"]
+  }).filter(it => it.score > -3000).map(it => it.obj);
+}
+
+
+
+function handleFilesValidated(result: FsValidationResult, files: File[]) {
+  // console.log('Validation result: ' + result);
+}
+
+
+
+function handleFilesChanged(files: File[]) {
+  const newReport = files[0];
+
+  getBase64(newReport).then(
+    res => {
+      student.value.pfp = res.substring(22)
+      updateGeneralStudent()
+    }
+  )
+}
+
+
+
+
+
+onMounted(() => {
+  sid.value = (route.params.id as string)
+  loadStudent(sid.value).then (
+    res => {
+      student.value = res
+    }
+  )
+  loadProjects(sid.value).then(res => projects.value = res)
+
+  loadCoauthors(sid.value).then(res => coauthors.value = res)
+
+})
+
+</script>
+<style>
+.v-card__text, .v-card__title {
+  word-break: normal; /* maybe !important  */
+}
+
+a {
+    text-decoration: none;
+    color: #1867C0;
+    /* font-weight: bold; */
+}
+</style>
