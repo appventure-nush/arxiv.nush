7d1192547d1b03f134c9c8814003a0b3d65e47c9
Prannaya <prannayagupta@gmail.com>
2023-04-17 11:21:25 +0800

    rearrange whole repo

--- submission/frontend/src/api/journals.ts
+++
@@ -1,6 +0,0 @@
-import { API_URL } from "./api_constants";
-import { Journal } from "@/types/publication";
-
-export async function searchJournals(): Promise<Journal[]> {
-  return await(await fetch(`${API_URL}/search/journals`)).json()
-}
--- submission/frontend/src/types/api_types.ts
+++
@@ -1,7 +0,0 @@
-import { Student, Teacher } from "@/types/user";
-
-export class AuthResult {
-  result!: boolean
-  message!: string
-  user!: Student | Teacher
-}
